7875b9a45f683f8fca0f6a06c6c8ba4b79a07526
SpecialistSteak <mluke25@aischennai.org>
2024-12-15 20:31:32 +0530

    feat: add favicon and update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,19 @@
 import "./globals.css";
+import Head from 'next/head';
 
-let title = "Hibbard's Custard Notifier";
-let description =
+const title = "Hibbard's Custard Notifier";
+const description =
   "This is a custard notifier webapp made to notify you whenever a flavor you're interested in at Hibbard's comes up.";
 
 export const metadata = {
   title,
   description,
   metadataBase: new URL("https://hibbards-custard-alerter.vercel.app/"),
+  icons: {
+    icon: '/static/favicon.ico', 
+    shortcut: '/static/favicon-16x16.png',
+    apple: '/static/apple-touch-icon.png',
+  },
 };
 
 export default function RootLayout({
@@ -17,6 +23,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+    <Head>
+    <link rel="shortcut icon" href="/static/favicon.ico" />
+		</Head>
       <body className="bg-gradient-light from-custom-light-from to-custom-light-to dark:bg-gradient-dark dark:from-custom-dark-from dark:to-custom-dark-to min-h-screen">
         {children}
       </body>
